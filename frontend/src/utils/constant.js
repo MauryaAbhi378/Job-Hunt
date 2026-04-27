c97e9c07e0434b06d73fc3375065752f2ccd10b8
MauryaAbhi378 <abhim0505@gmail.com>
2026-04-27 11:40:21 +0530

    Revert "url changed"
    This reverts commit b262bf2ce573ce8fd85d712897d21e03869f538c.

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,6 +1,5 @@
-export const USER_API_ENDPOINT = "https://job-hunt-yjwx.onrender.com/api/user";
-export const JOB_API_ENDPOINT = "https://job-hunt-yjwx.onrender.com/api/job";
+export const USER_API_ENDPOINT = "http://localhost:5000/api/user";
+export const JOB_API_ENDPOINT = "http://localhost:5000/api/job";
 export const APPLICATION_API_END_POINT =
-  "https://job-hunt-yjwx.onrender.com/api/application";
-export const COMPANIES_API_ENDPOINT =
-  "https://job-hunt-yjwx.onrender.com/api/company";
+  "http://localhost:5000/api/application";
+export const COMPANIES_API_ENDPOINT = "http://localhost:5000/api/company";
